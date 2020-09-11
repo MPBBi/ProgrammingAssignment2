c598e5f8c81b534b113333db9d15b4e43659c88b
MPBBi <mya.pham@outlook.com>
2020-09-11 23:06:36 +1000

    edited and completed for assignment 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## we will be accepting a matrix, then inversing it and caching it
+
+
+#cacheSolve(makeCacheMatrix(matrix(c(1:4),2,2)))
+
+## numeric() creates a real vector of a specified length,
+## the elments of this vector are all equal to 0
+## by default 
+#########################################################
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                 i <- NULL
+                 set <- function(y) {
+                 x <<- y
+                 i <<- NULL
+                     }
+       get <- function() x  
+       setinverse <- function(solve) i <<- solve
+       getinverse <- function() i
+       list(set = set, get = get,
+            setinverse = setinverse,
+            getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## we will be checking if inverse exists yet and if it does pull that
+## else if it doesnt then we will comput the inverse. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+           i <- x$getinverse()
+          if(!is.null(i)) {
+            message("getting cached data")
+             return(i)
+              }
+           data <- x$get()
+           i <- solve(data, ...)
+           x$setinverse(i)
+           i
 }
